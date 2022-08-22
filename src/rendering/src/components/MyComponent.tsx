a543ad3cbec9aa435481671e0176ef60682fe56a
ivanbuzyka <ivanbuzyka@gmail.com>
2022-08-22 16:13:37 +0200

    fixed prettier issues

--- src/rendering/src/components/MyComponent.tsx
+++ src/rendering/src/components/MyComponent.tsx
@@ -1,17 +1,19 @@
 import { Text, Field, RichText } from '@sitecore-jss/sitecore-jss-nextjs';
 
 type MyComponentProps = {
-    fields: {
-        heading: Field<string>;
-        body: Field<string>;
-    };
+  fields: {
+    heading: Field<string>;
+    body: Field<string>;
+  };
 };
 
 const MyComponent = (props: MyComponentProps): JSX.Element => (
-    <div>
-        <span><Text field={props.fields.heading} /></span>
-        <RichText field={props.fields.body} />
-    </div>
+  <div>
+    <span>
+      <Text field={props.fields.heading} />
+    </span>
+    <RichText field={props.fields.body} />
+  </div>
 );
 
 export default MyComponent;
